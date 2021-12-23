a52bd2d78f5f781a9fa442e67398253202d55612
Jed-Tapiador <doom.slaver2@gmail.com>
2021-12-23 20:11:15 +0800

    enable rainbow and grayscale buttons. simplify code

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
 function createGrid(sides){
     for(let x = 0; x < sides*sides; x++){
-        const x = document.createElement("div");
+        let x = document.createElement("div");
         x.classList.add("cell");
         x.setAttribute("style",`width: ${450/sides}px; height: ${450/sides}px;`);
+        x.addEventListener('mouseover', () => mouseOver(x));
         container.appendChild(x);
     }
 }
 
-function mouseOver(color){
-    cells.forEach((cell) => {
-        cell.addEventListener("mouseover", () => cell.style.backgroundColor = color);
-    });
+function mouseOver(x){
+    if(rainbow.disabled == true){
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255); 
+        x.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.5)`;
+    } else {
+        const a = Math.random();
+        x.style.backgroundColor = `rgba(0, 0, 0, ${a})`;
+    }
 }
 
+function wipeout(){
+    while(container.firstChild) container.removeChild(container.firstChild);
+    createGrid(sides);
+    let cells = document.querySelectorAll(".cell");
+}
+
+
 //Initial values
-let sides = 16;
-let color = "blue";
+const container  = document.querySelector(".container");
+const clear      = document.querySelector(".clear");
+const newGrid    = document.querySelector(".new");
+const rainbow    = document.querySelector(".rainbow");
+const grayscale  = document.querySelector(".grayscale");
+let sides        = 16;
+rainbow.disabled = true;
 
 //Create initial 16x16 grid
-const container = document.querySelector(".container");
 createGrid(sides);
 let cells = document.querySelectorAll(".cell");
-mouseOver(color);
-
 
 //Clear button
-const clear = document.querySelector(".clear");
-clear.addEventListener("click", () => {
-    cells.forEach((cell) => { 
-        cell.style.backgroundColor = "white";
-    });
-});
+clear.addEventListener("click", () => wipeout());
 
 //New Grid button
-const newGrid = document.querySelector(".new");
 newGrid.addEventListener("click", () => {
     do{
         sides = prompt("What would be the side of the grid? (Note: Only accepts a number from 1 to 50)");
     } while (sides > 50 || sides < 1)
-    while(container.firstChild){
-        container.removeChild(container.firstChild);
-    }
-    createGrid(sides);
-    cells = document.querySelectorAll(".cell");
-    mouseOver(color);
+    wipeout();
 })
+
+//Rainbow button
+rainbow.addEventListener("click", () => {
+    rainbow.disabled = true;
+    grayscale.disabled = false;
+});
+
+//Grayscale button
+grayscale.addEventListener("click", () => {
+    rainbow.disabled = false;
+    grayscale.disabled = true;
+});
