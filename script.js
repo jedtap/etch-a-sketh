5026d3a712964fcf98a1bf05e82995a4fb362917
Jed-Tapiador <doom.slaver2@gmail.com>
2021-12-23 14:17:47 +0800

    add clear and new grid features

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
-
-
-/*
-
-//create 16x16 grid
-const container = document.querySelector(".container");
-for(let x = 0; x < 16*16; x++){
-    const x = document.createElement("div");
-    x.classList.add("cell");
-    x.textContent = "A";
-    container.appendChild(x);
+function createGrid(sides){
+    for(let x = 0; x < sides*sides; x++){
+        const x = document.createElement("div");
+        x.classList.add("cell");
+        x.setAttribute("style",`width: ${450/sides}px; height: ${450/sides}px;`);
+        container.appendChild(x);
+    }
 }
 
+function mouseOver(color){
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", () => cell.style.backgroundColor = color);
+    });
+}
 
-let selection = 0;
+//Initial values
+let sides = 16;
+let color = "blue";
+
+//Create initial 16x16 grid
+const container = document.querySelector(".container");
+createGrid(sides);
+let cells = document.querySelectorAll(".cell");
+mouseOver(color);
 
 
+//Clear button
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    cells.forEach((cell) => { 
+        cell.style.backgroundColor = "white";
+    });
+});
 
-
-//button press
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-
-    selection = prompt("side of square?");
-
+//New Grid button
+const newGrid = document.querySelector(".new");
+newGrid.addEventListener("click", () => {
+    do{
+        sides = prompt("What would be the side of the grid? (Note: Only accepts a number from 1 to 50)");
+    } while (sides > 50 || sides < 1)
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
-
-    for(let x = 0; x < selection*selection; x++){
-        const x = document.createElement("div");
-        x.classList.add("cell");
-        x.textContent = "A";
-        container.appendChild(x);
-    }
-    
-//mouseover color changing
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => cell.setAttribute("style","background-color: blue"));
-});
-
-
-});
-
-
-
-//mouseover color changing
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => cell.setAttribute("style","background-color: blue"));
-});
-
-
-//random color tools
-//opacity tools
-
-*/
+    createGrid(sides);
+    cells = document.querySelectorAll(".cell");
+    mouseOver(color);
+})
