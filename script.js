e82741dc541115befd323bae31a7c3dfd3ebb6d4
Jed-Tapiador <doom.slaver2@gmail.com>
2021-12-23 12:34:59 +0800

    complete HTML and CSS files. disconnect JS file

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
+
+
+/*
+
 //create 16x16 grid
 const container = document.querySelector(".container");
-for(let x = 0; x<16*16; x++){
+for(let x = 0; x < 16*16; x++){
     const x = document.createElement("div");
+    x.classList.add("cell");
     x.textContent = "A";
     container.appendChild(x);
 }
 
+
+let selection = 0;
+
+
+
+
+//button press
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+
+    selection = prompt("side of square?");
+
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    for(let x = 0; x < selection*selection; x++){
+        const x = document.createElement("div");
+        x.classList.add("cell");
+        x.textContent = "A";
+        container.appendChild(x);
+    }
+    
+//mouseover color changing
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => cell.setAttribute("style","background-color: blue"));
+});
+
+
+});
+
+
+
+//mouseover color changing
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => cell.setAttribute("style","background-color: blue"));
+});
+
+
+//random color tools
+//opacity tools
+
+*/
